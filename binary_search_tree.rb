8da642d2dd05d42f3a20b266036aa8966006c799
Vaughn Antonyan <vaughnanton@gmail.com>
2017-08-17 22:02:27 -0700

    binary tree build method complete

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,1 +1,31 @@
-#initialize binary search tree file
+class Node
+  attr_reader :value
+  attr_accessor :left_node, :right_node
+
+  def initialize(value = nil)
+    @value = value
+  end
+
+  def insert(new_value)
+    if new_value < value
+      left_node.nil? ? self.left_node = Node.new(new_value) : left_node.insert(new_value)
+    else
+      right_node.nil? ? self.right_node = Node.new(new_value) : right_node.insert(new_value)
+    end
+  end
+end
+
+def build_tree
+  root = Node.new(array.shift)
+  array.each { |v| root.insert(v) }
+  root
+end
+
+def breadth_first_search
+end
+
+def depth_first_search
+end
+
+def dfs_rec
+end
